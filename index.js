896b1c1d363cdda1ad0a2304a1612fcf1c5f7083
Ralph Molu <rmolu11@gmail.com>
2024-04-05 14:23:11 -0400

    destructures the response object, made use of filesystemt po generate the README file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('filesystem');
+const { generate } = require('rxjs');
 
-const userQuestions = [
+inquirer.prompt([
     {
         type: 'input',
         name: 'title',
@@ -16,7 +17,10 @@ const userQuestions = [
         type: 'input',
         name: 'features',
         message: 'List the features of your application separated by a comma',
-        filter: input => input.split(',')
+        filter: function(input){ return input.split(',').map(function(feature){
+            return feature.trim();
+        })
+        }
     },
     {
         type: 'input',
@@ -35,15 +39,30 @@ const userQuestions = [
     },
     {
         type: 'input',
-        name: 'credits',
-        message: 'List your collaborators if any'
+        name: 'github',
+        message: 'Insert the link to your Github repo'
     },
-    
     {
         type: 'input',
-        name: 'credits',
-        message: 'List your collaborators if any'
+        name: 'linkedin',
+        message: 'Insert the link to your LinkedIn profile'
     },
     
     
-]
+])
+.then((response) =>{
+
+    const { title, description, features, installation, usage, credits, github, linkedin} = response;
+
+    const readMeContent = `
+    ${response.title}
+    
+    Description
+
+    `
+   
+
+    fs.writeFile('README.md', readMeContent, (error) => {
+        error ? console.log(error) : console.log(response)
+    })
+});
